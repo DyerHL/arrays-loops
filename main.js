b09366250c3aa8111e81464ccd205f6d336066d4
Halie Dyer <Halie.L.Dyer@gmail.com>
2021-07-20 20:57:59 -0500

    added quotation around img address

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("Ready");
 
 let group6 = [
   {
-    img: https://avatars.githubusercontent.com/u/86807857?v=4,
+    img: "https://avatars.githubusercontent.com/u/86807857?v=4", 
     id: 1,
     name: "Nissa",
     email: "Nissa@nissan.org",
@@ -10,7 +10,7 @@ let group6 = [
     unique: "likes stilletos",
   },
   {
-    img: https://avatars.githubusercontent.com/u/82732748?v=4,
+    img: "https://avatars.githubusercontent.com/u/82732748?v=4", 
     id: 2,
     name: "Daniel",
     email: "daniel@email.com",
@@ -18,7 +18,7 @@ let group6 = [
     unique: "builds PCs",
   },
   {
-    img: https://avatars.githubusercontent.com/u/86806913?v=4,
+    img: "https://avatars.githubusercontent.com/u/86806913?v=4",
     id: 3,
     name: "Halie",
     email: "halie@gmail.com",
@@ -26,7 +26,7 @@ let group6 = [
     unique: "allergic to Advil",
   },
   {
-    img: https://avatars.githubusercontent.com/u/86626953?v=4,
+    img: "https://avatars.githubusercontent.com/u/86626953?v=4",
     id: 4,
     name: "Klay",
     email: "Klay@gmail.com",
