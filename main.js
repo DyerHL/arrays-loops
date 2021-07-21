9ef3d2489ad69c89ba3ab37d4c8fece3cf90f9bb
Halie Dyer <Halie.L.Dyer@gmail.com>
2021-07-20 20:54:48 -0500

    added object

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 console.log("Ready");
+
+let group6 = [
+  {
+    img: https://avatars.githubusercontent.com/u/86807857?v=4,
+    id: 1,
+    name: "Nissa",
+    email: "Nissa@nissan.org",
+    favColor: "pink",
+    unique: "likes stilletos",
+  },
+  {
+    img: https://avatars.githubusercontent.com/u/82732748?v=4,
+    id: 2,
+    name: "Daniel",
+    email: "daniel@email.com",
+    favColor: "Teal",
+    unique: "builds PCs",
+  },
+  {
+    img: https://avatars.githubusercontent.com/u/86806913?v=4,
+    id: 3,
+    name: "Halie",
+    email: "halie@gmail.com",
+    favColor: "green",
+    unique: "allergic to Advil",
+  },
+  {
+    img: https://avatars.githubusercontent.com/u/86626953?v=4,
+    id: 4,
+    name: "Klay",
+    email: "Klay@gmail.com",
+    favColor: "purple",
+    unique: "has hearing loss",
+  },
+];  
